6c72dbc3e578a0c1fac91c02381c3e36eba32828
Steven Bister <steve.bister@openstudycollege.com>
2024-06-18 11:56:20 +0100

    Scaffold Storefront

---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import {defineConfig} from 'vite';
+import {hydrogen} from '@shopify/hydrogen/vite';
+import {oxygen} from '@shopify/mini-oxygen/vite';
+import {vitePlugin as remix} from '@remix-run/dev';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  plugins: [
+    hydrogen(),
+    oxygen(),
+    remix({presets: [hydrogen.preset()]}),
+    tsconfigPaths(),
+  ],
+  ssr: {
+    optimizeDeps: {
+      include: ['typographic-base'],
+    },
+  },
+  optimizeDeps: {
+    include: [
+      'clsx',
+      '@headlessui/react',
+      'typographic-base',
+      'react-intersection-observer',
+      'react-use/esm/useScroll',
+      'react-use/esm/useDebounce',
+      'react-use/esm/useWindowScroll',
+    ],
+  },
+  build: {
+    // Allow a strict Content-Security-Policy
+    // withtout inlining assets as base64:
+    assetsInlineLimit: 0,
+  },
+});
